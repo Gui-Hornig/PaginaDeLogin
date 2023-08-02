f4d9ade8d63b9da213c32b8be1d5b4c4c2d03a14
Gui-hornig <guihornig@gmail.com>
2023-08-02 17:38:42 -0300

    inicio

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function validateFields() {
     const email = document.getElementById("email").value;
     const password = document.querySelector('input[type="password"]').value;
+    const recoverPasswordLink = document.querySelector('.remember-forgot-a');
+    const loginButton = document.querySelector('.btn');
